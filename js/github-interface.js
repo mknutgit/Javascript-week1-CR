dd0ac1e930e79374392ed044da71ad73e495bd8b
Matt <mknut001@gmail.com>
2016-07-12 17:01:31 -0700

    revert last commit

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,22 +1,19 @@
-
-var getRepos = require('./../js/github.js').getRepos;
-var getUser = require('./../js/github.js').getUser;
-var listRepos = require('./../js/display.js').listRepos;
-var showUserInfo = require('./../js/display.js').showUserInfo;
+var apiKey = require('./../.env').apiKey;
+var repos = require('./../js/github.js');
+var user = require('./../js/github.js');
 
 $(document).ready(function() {
-
   $('#repos').hide();
 
-  $('#repo').click(function(event) {
-    event.preventDefault();
-    getRepos($('#username').val(), listRepos );
-  });
+  $('#repo').click(function() {
+    $('#repos').show();
 
-  $('#repo').click(function(event) {
-    event.preventDefault();
-    getUser($('#username').val(), showUserInfo );
-      $('#repos').show();
-  });
+    var username = $('#username').val();
 
+    $('#username').val('');
+
+    user.getUser(username);
+    repos.getRepos(username);
+
+  });
 });
